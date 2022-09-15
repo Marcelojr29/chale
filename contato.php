4cf553cfaa5ac1ec06a60d9081be6bb78468ee73
MarceloJunior <marcelojr2904@gmail.com>
2022-09-14 23:10:54 -0400

    atualizados

---
+++ contato.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contato</title>
+	<link rel="shortcut icon" type="text/css" href="imagens/logo.png">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+	<div id="container"><!-- Inicio do container -->
+
+		<div id="topo">
+			<div id="area-logo">
+				<h1 class="logo">
+					<a href=""> Chalé Hotel</a>
+				</h1>
+
+			</div>
+			<div id="area-menu">
+				<div id="conteudo-menu">
+					<div id="menu-locais">
+						<span class="locais"> Manaus - Amazonas</span>
+						<a href="" class="reserva"> Reserva</a>
+						<a href="" class="reserva"> Login</a>
+						<div style="clear: both;"></div>
+
+					</div>
+					<div id="menu">
+						<ul id="navegacao">
+							<li><a href="index.php" target="_blank">Home</a></li>
+							<li><a href="historia.php" target="_blank">História</a></li>
+							<li><a href="imprensa.php" target="_blank">Imprensa</a></li>
+							<li><a href="gastronomia.php" target="_blank">Gastronomia</a></li>
+							<li><a href="contato.php">Contato</a></li>
+						</ul>
+						<img class="depoimento" src="imagens\hotel.png" width="150px">
+					</div>
+					
+				</div>
+
+			</div>
+
+		</div>
+		<div id="area-principal">
+			<div class="conteudo">
+				<h2>Lorem ipsum dolor sit amet, consectetur</h2>
+				<p class="text-principal"></p>
+				<p class="text-principal"></p>
+				<p class="text-principal"></p>
+			</div>
+			
+		</div>
+		<div id="area-lateral">
+			<div class="conteudo">
+				<ul id="beneficios">
+					<li> 
+						<a href="">
+							<img src="imagens/ap.png" width="150" alt="Apartamento">
+							<h3>Apartamento</h3>
+							<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+						</a>
+					</li>
+					<li> 
+						<a href="">
+							<img src="imagens/rest.jpg" width="150" alt="Restaurante">
+							<h3>Restaurante</h3>
+							<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+						</a>
+					</li>
+					<li> 
+						<a href="">
+							<img src="imagens/pisc.jpg" width="150" alt="Piscina">
+							<h3>Piscina</h3>
+							<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+						</a>
+					</li>
+				</ul>
+				
+			</div>
+			
+		</div>
+		<div id="rodape">
+			<span>
+				&copy;Copyright 2000-2020 Chalé Hotel
+			</span>
+			
+		</div>
+
+	</div>
+
+</body>
+
+</html>
---
+++ historia.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Historia</title>
+	<link rel="shortcut icon" type="text/css" href="imagens/logo.png">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+	<div id="container"><!-- Inicio do container -->
+
+		<div id="topo">
+			<div id="area-logo">
+				<h1 class="logo">
+					<a href=""> Chalé Hotel</a>
+				</h1>
+
+			</div>
+			<div id="area-menu">
+				<div id="conteudo-menu">
+					<div id="menu-locais">
+						<span class="locais"> Manaus - Amazonas</span>
+						<a href="" class="reserva"> Reserva</a>
+						<a href="" class="reserva"> Login</a>
+						<div style="clear: both;"></div>
+
+					</div>
+					<div id="menu">
+						<ul id="navegacao">
+							<li><a href="index.php" target="_blank">Home</a></li>
+							<li><a href="historia.php">História</a></li>
+							<li><a href="imprensa.php" target="_blank">Imprensa</a></li>
+							<li><a href="gastronomia.php" target="_blank">Gastronomia</a></li>
+							<li><a href="contato.php" target="_blank">Contato</a></li>
+						</ul>
+						<img class="depoimento" src="imagens\hotel.png" width="150px">
+					</div>
+					
+				</div>
+
+			</div>
+
+		</div>
+		<div id="area-principal">
+			<div class="conteudo">
+				<h2>Lorem ipsum dolor sit amet, consectetur</h2>
+				<p class="text-principal"></p>
+				<p class="text-principal"></p>
+				<p class="text-principal"></p>
+			</div>
+			
+		</div>
+		<div id="rodape">
+			<span>
+				&copy;Copyright 2000-2020 Chalé Hotel
+			</span>
+			
+		</div>
+
+	</div>
+
+</body>
+
+</html>
